9d1f222aeb8332a1242200b391625d896df46966
akshay-puranik <akshay.social@gmail.com>
2022-09-28 12:10:05 +0530

    dash nav inputs added

--- src/Pages/Dashboard/Components/DashNav.jsx
+++ src/Pages/Dashboard/Components/DashNav.jsx
@@ -1,4 +1,5 @@
-import { Center, Input, MenuOptionGroup, Select, Spacer, Stack, Text } from '@chakra-ui/react';
+import { ArrowBackIcon, ArrowForwardIcon } from '@chakra-ui/icons';
+import { Box, Button, ButtonGroup, Center, Flex, HStack, Input, MenuOptionGroup, Select, Spacer, Stack, Text } from '@chakra-ui/react';
 import React from 'react';
 
 const DashNav = () => {
@@ -17,7 +18,15 @@ const DashNav = () => {
           <option value='only_me'>Only Me</option>
           <option value='team'>Team</option>
         </Select>
-        <Input type={"date"} />
+        <HStack>
+          {/* <Input type={"date"} /> */}
+          <Center border="1px solid" h="40px" w="40px" >
+            <ArrowBackIcon />
+          </Center>
+          <Center border="1px solid" h="40px" w="40px" >
+            <ArrowForwardIcon />
+          </Center>
+        </HStack>
       </Stack>
     </Stack>
   );
